e58e82c4582f4274596ca28c79f58a0edd22d6fb
HEINRICA <Albert.Heinrichs@kapsch.net>
2025-07-11 10:09:18 +0200

    v0, not working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-grpc server implemented using the vertx-grpc dependency. For performance comparison vs. vanilla java grpc and go grpc implementations.
+grpc server implemented using the vertx-grpc dependency. 
+For performance comparison vs. vanilla java grpc and go grpc implementations.
---
+++ src/main/java/com/alheijn/java_vertx_grpc_server/VertxGrpcServer.java
@@ -0,0 +1,49 @@
+package com.alheijn.java_vertx_grpc_server;
+
+import com.ecommerce.ordermanagement.OrderManagement;
+import com.ecommerce.ordermanagement.OrderManagementGrpc;
+import com.ecommerce.ordermanagement.OrderManagementGrpcService;
+import com.ecommerce.ordermanagement.OrderManagementService;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.grpc.server.GrpcServer;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VertxGrpcServer extends AbstractVerticle {
+
+    private static final Logger logger = Logger.getLogger(VertxGrpcServer.class.getName());
+
+    @Autowired
+    OrderManagementServiceImpl service;
+
+    @Value("${grpc.server.port}")
+    private int port;
+
+    @Override
+    public void start() throws Exception {
+        logger.info("Creating grpc server");
+
+        // create the server
+        GrpcServer rpcServer = GrpcServer.server(vertx);
+        rpcServer.callHandler(OrderManagementGrpcService.CreateOrder, request -> {
+            request
+                .last()
+                .onSuccess(order -> {
+                    service.createOrder(order);
+                });
+        });
+
+        // start the server
+        vertx.createHttpServer().requestHandler(rpcServer).listen(port)
+            .onFailure(Throwable::printStackTrace);
+        logger.info("Started grpc server on port " + port);
+    }
+
+
+
+
+
+}
