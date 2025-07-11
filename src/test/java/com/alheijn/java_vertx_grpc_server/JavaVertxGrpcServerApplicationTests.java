a61dbe69a1fd01daa6bceede63aaae2b065053b1
alheijn <albert.heinrichs.shop@gmail.com>
2025-07-11 07:42:49 +0200

    initial commit

---
+++ src/test/java/com/alheijn/java_vertx_grpc_server/JavaVertxGrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alheijn.java_vertx_grpc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVertxGrpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
