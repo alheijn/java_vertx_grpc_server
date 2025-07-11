f932d336ad40ee609b2f7573f9b64fc66d22ecc6
HEINRICA <Albert.Heinrichs@kapsch.net>
2025-07-11 12:56:15 +0200

    Revert "v0, not working"
    This reverts commit e58e82c4582f4274596ca28c79f58a0edd22d6fb.

--- src/main/java/com/alheijn/java_vertx_grpc_server/JavaVertxGrpcServerApplication.java
+++ src/main/java/com/alheijn/java_vertx_grpc_server/JavaVertxGrpcServerApplication.java
@@ -1,25 +1,13 @@
 package com.alheijn.java_vertx_grpc_server;
 
-import io.vertx.core.Vertx;
-import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaVertxGrpcServerApplication {
 
-    @Autowired
-    private VertxGrpcServer vertxGrpcServer;
-
     public static void main(String[] args) {
         SpringApplication.run(JavaVertxGrpcServerApplication.class, args);
-    }
-
-    @PostConstruct
-    public void deployVerticle() {
-        Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(vertxGrpcServer);
     }
 
 }
